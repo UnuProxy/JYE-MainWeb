8abee73bf315f576b8a4ca5383f68941e9b7dbbc
UnuProxy <129114953+UnuProxy@users.noreply.github.com>
2025-01-16 14:32:24 +0100

    00

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
 });
-const db = admin.firestore(); // Initialize Firestore
+const db = admin.firestore(); 
 
 // Middleware
 const allowedOrigins = ['http://localhost:3000', 'https://your-production-url.com'];
